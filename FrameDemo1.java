88d7dcc7ba101c6c56da4d28844c1d7ee45538f0
SonarJayesh <jayeshsonar2824@gmail.com>
2024-05-22 16:04:29 +0530

    [ADDED]: JAYESH java code for Creating a Simple Frame in AWT Swing

---
+++ FrameDemo1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class FrameDemo1{
+	public static void main(String[] args){
+	
+		Frame frame = new Frame();
+		frame.setSize(400,400);
+		frame.setLocation(100,100);
+		frame.setTitle("MyFrame");
+		frame.setVisible(true);
+	}
+}
